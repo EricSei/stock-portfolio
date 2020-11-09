fb704e065d897d9b12fdfaac555bd6120ce4f7f3
ericsei <eric.ahhpusei@gmail.com>
2020-11-09 14:08:48 -0500

    refactored envorimental changed tested

--- client/src/context/AuthContex.js
+++ client/src/context/AuthContex.js
@@ -1,16 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const AuthContext = React.createContext();
 
-export const AuthProvider = ({ children })=> {
+export const AuthProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false);
 
-    const [isAuth, setIsAuth ] = useState(false);
-
-    return (
-        <AuthContext.Provider value={{isAuth, setIsAuth }}>
-            {children}
-        </AuthContext.Provider>
-    );  
-}
+  return (
+    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
